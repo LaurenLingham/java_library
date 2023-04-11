0d3daf487d10f74496b34e2dd888d86b3bb3643f
LaurenLingham <lauren.lingham@outlook.com>
2023-04-11 20:27:49 +0100

    library checks for title and stores qty of genre

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -3,17 +3,11 @@ import java.util.HashMap;
 
 public class Library {
 
-    private ArrayList<Book> books;
-    private int capacity;
-    private HashMap<String, Integer> booksByGenre;
+    private ArrayList<Book> books = new ArrayList<>();
+    private int capacity = 10;
+    private HashMap<String, Integer> booksByGenre = new HashMap<>();
 
-    public Library() {
-        this.books = new ArrayList<Book>();
-        this.capacity = 50;
-        this.booksByGenre = new HashMap<>();
-    }
-
-    public int getBooks() {
+    public int getNumberOfBooks() {
         return this.books.size();
     }
 
@@ -21,12 +15,27 @@ public class Library {
         return this.capacity;
     }
     public void addBook(Book book) {
-        this.books.add(book);
-        this.booksByGenre.put(book.getGenre(), 1);
+        if (getNumberOfBooks() < this.capacity) {
+            this.books.add(book);
+            this.booksByGenre.put(book.getGenre(), 1);
+        }
     }
 
     public int getBooksByGenre(String fiction) {
         int numberOfBooksByGenre = booksByGenre.get(fiction);
         return numberOfBooksByGenre;
+    }
+
+    public boolean hasBookWithTitle(String title) {
+        for (Book book : books) {
+            if (book.getTitle().equals(title)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public HashMap<String, Integer> getNumberOfBooksByGenre() {
+        return booksByGenre;
     }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,6 +1,8 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
+
 import static org.junit.Assert.assertEquals;
 
 public class LibraryTest {
@@ -11,29 +13,48 @@ public class LibraryTest {
     @Before
     public void before() {
         library = new Library();
+        book = new Book("The Wasp Factory", "Iain Banks", "fiction");
     }
 
     @Test
     public void hasCapacity() {
-        assertEquals(50, library.getCapacity());
+        assertEquals(10, library.getCapacity());
     }
 
     @Test
     public void startsWithNoBooks() {
-        assertEquals(0, library.getBooks());
+        assertEquals(0, library.getNumberOfBooks());
     }
 
     @Test
     public void canAddBook() {
-        book = new Book("The Wasp Factory", "Iain Banks", "fiction");
         library.addBook(book);
-        assertEquals(1, library.getBooks());
+        assertEquals(1, library.getNumberOfBooks());
     }
 
     @Test
     public void addsBookToGenreList() {
-        book = new Book("The Wasp Factory", "Iain Banks", "fiction");
         library.addBook(book);
         assertEquals(1, library.getBooksByGenre("fiction"));
+    }
+
+    @Test
+    public void canCheckForTitleInStockTrue() {
+        library.addBook(book);
+        assertEquals(true, library.hasBookWithTitle("The Wasp Factory"));
+    }
+
+    @Test
+    public void canCheckForTitleInStockFalse() {
+        library.addBook(book);
+        assertEquals(false, library.hasBookWithTitle("Batman"));
+    }
+
+    @Test
+    public void canGetNumberOfBooksByGenre() {
+        library.addBook(book);
+        HashMap<String, Integer> expected = new HashMap<>();
+        expected.put("fiction", 1);
+        assertEquals(expected, library.getNumberOfBooksByGenre());
     }
 }
